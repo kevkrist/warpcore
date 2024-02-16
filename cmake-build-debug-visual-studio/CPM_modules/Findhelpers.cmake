79a3862b669ede48029cde950e02ccbb006a2ea0
Kevin Kristensen <kckristensen@wisc.edu>
2024-02-16 08:38:37 -0600

    BF retrieve_write implemented

---
+++ cmake-build-debug-visual-studio/CPM_modules/Findhelpers.cmake
@@ -0,0 +1,3 @@
+include(C:/Users/polyn/Code/warpcore/cmake-build-debug-visual-studio/cmake/CPM_0.27.3.cmake)
+CPMAddPackage(NAME;helpers;URL;https://gitlab.rlp.net/pararch/hpc_helpers/-/archive/restructure/hpc_helpers-restructure.zip;DOWNLOAD_EXTRACT_TIMESTAMP;TRUE)
+set(helpers_FOUND TRUE)
---
+++ cmake-build-debug-visual-studio/CPM_modules/Findkiss_rng.cmake
@@ -0,0 +1,3 @@
+include(C:/Users/polyn/Code/warpcore/cmake-build-debug-visual-studio/cmake/CPM_0.27.3.cmake)
+CPMAddPackage(NAME;kiss_rng;URL;https://github.com/sleeepyjack/kiss_rng/archive/refs/heads/master.zip;DOWNLOAD_EXTRACT_TIMESTAMP;TRUE)
+set(kiss_rng_FOUND TRUE)
